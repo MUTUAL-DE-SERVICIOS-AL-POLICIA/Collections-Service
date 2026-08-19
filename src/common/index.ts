12f7a89b9a18db72dd68f0d74329282464182182
Fercho <110748347+ferbautistamollo@users.noreply.github.com>
2026-08-19 12:15:33 -0400

    Revert "create table bank_statement and add import excel"

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,6 @@
 export { PaginationDto } from './dtos/pagination.dto';
 
 export { NatsService } from './nats/nats.service';
-export { ImportProcessorService } from './import/import-processor.service';
 
 export { RpcCustomExceptionFilter } from './exceptions/rpc-custom-exception.filter';
 export { BadRequestCustomExceptionFilter } from './exceptions/bad-request-custom-exception.filter';
