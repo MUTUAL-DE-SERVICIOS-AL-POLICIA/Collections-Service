5a3f35b545ade5a8b71ca4acdebd3aa6e50571c1
ferbautistamollo <ferbautistamollo@gmail.com>
2026-09-01 01:50:23 -0400

    ajustes de importacion nombre

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
-import { CollectionsModule } from './collections/collections.module';
-import { BankStatementModule } from './bank-statement/bank-statement.module';
+import { CollectionsModule } from './transactions/transactions.module';
+import { BankStatementModule } from './bank-statements/bank-statements.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, CommonModule, CollectionsModule, BankStatementModule],
